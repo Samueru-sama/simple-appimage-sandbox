4a20a44e423eb44b961877ee19c5c56bc668af82
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-05-29 00:30:08 +0000

    Add files via upload

---
+++ sas.sh
@@ -0,0 +1,567 @@
+#!/bin/sh
+
+# simple appimage sandbox
+# sandbox appimages and appimage like formats easily
+# can also sandbox standalone binaries and similar
+
+# THIS PRODUCT COMES WITH NO WARRANTY!
+
+set -e
+
+VERSION=0.1
+
+ADD_DIR=""
+ALLOW_BINDIR=0
+ALLOW_DATADIR=0
+ALLOW_CONFIGDIR=0
+ALLOW_CACHEDIR=0
+ALLOW_STATEDIR=0
+ALLOW_RUNDIR=0
+ALLOW_APPLICATIONSDIR=0
+ALLOW_DESKTOPDIR=0
+ALLOW_DOCUMENTSDIR=0
+ALLOW_DOWNLOADDIR=0
+ALLOW_GAMESDIR=0
+ALLOW_MUSICDIR=0
+ALLOW_PICTURESDIR=0
+ALLOW_PUBLICSHAREDIR=0
+ALLOW_TEMPLATESDIR=0
+ALLOW_VIDEOSDIR=0
+
+SHARE_APP_CONFIG=1
+SHARE_APP_THEME=1
+SHARE_APP_NETWORK=1
+SHARE_APP_AUDIO=1
+SHARE_APP_DBUS=1
+SHARE_APP_XDISPLAY=1
+SHARE_APP_WDISPLAY=1
+SHARE_APP_PIPEWIRE=1
+
+SQUASHFS_APPIMAGE=0
+DWARFS_APPIMAGE=0
+TARGET=""
+MOUNT_POINT=""
+
+# TODO Find a way to do this without readelf
+DEPENDENCIES="
+	awk
+	bwrap
+	cut
+	dwarfs
+	grep
+	head
+	id
+	md5sum
+	readelf
+	readlink
+	sed
+	squashfuse
+	tail
+	umount
+"
+
+# Name of variables and files to be checked
+XDG_BASE_DIRS="BINDIR CACHEDIR CONFIGDIR DATADIR RUNDIR STATEDIR"
+XDG_USER_DIRS="APPLICATIONSDIR DESKTOPDIR DOCUMENTSDIR
+	DOWNLOADDIR GAMESDIR MUSICDIR PICTURESDIR
+	PUBLICSHAREDIR TEMPLATESDIR VIDEOSDIR
+"
+
+DEFAULT_SYS_DIRS="
+	/bin
+	/etc
+	/lib
+	/lib32
+	/lib64
+	/opt
+	/sbin
+	/sys
+	/tmp
+	/usr
+"
+
+_cleanup() {
+	set +u
+	if [ -n "$MOUNT_POINT" ]; then
+		sleep 2
+		umount "$MOUNT_POINT"
+		rm -rf "$MOUNT_POINT"
+	fi
+	rm -f "$CONFIGDIR"/.user-dirs.dirs.safe
+}
+
+trap _cleanup INT TERM EXIT
+
+_help() {
+	printf '\n%s\n\n' "   USAGE: $0 [OPTIONS] /path/to/app"
+	exit 1
+}
+
+_error() {
+	>&2 printf '\n%s\n\n' "   💀 ERROR: $*"
+	exit 1
+}
+
+_dep_check() {
+	for d do
+		command -v "$d" 1>/dev/null || _error "Missing dependency $d"
+	done
+}
+
+# POSIX shell doesn't support arrays we use awk to save it into a variable
+# then with 'eval set -- $var' we add it to the positional array
+# see https://unix.stackexchange.com/questions/421158/how-to-use-pseudo-arrays-in-posix-shell-script
+_save_array() {
+	LC_ALL=C awk -v q="'" '
+	BEGIN{
+		for (i=1; i<ARGC; i++) {
+			gsub(q, q "\\" q q, ARGV[i])
+			printf "%s ", q ARGV[i] q
+		}
+		print ""
+	}' "$@"
+}
+
+_is_target() {
+	TARGET="$(readlink -f "$1")"
+	if [ -f "$TARGET" ]; then
+		APPNAME="$(basename "$TARGET")"
+		APP_APPIMAGE="$TARGET"
+		APP_ARGV0="$(basename "$1")"
+	else
+		return 1
+	fi
+}
+
+_is_spooky() {
+	case "$(readlink -f "$1")" in
+		""              |\
+		"/"             |\
+		"/home"         |\
+		"$HOME"         |\
+		"/run"          |\
+		"/dev"          |\
+		"/proc"         |\
+		"/mnt"          |\
+		"/media"        |\
+		~/.local        |\
+		~/.firefox      |\
+		~/.gnupg        |\
+		~/.mozilla      |\
+		~/.ssh          |\
+		~/.vim          |\
+		~/.bashrc       |\
+		~/.profile      |\
+		~/.bash_profile |\
+		~/.zshrc        |\
+		~/.zprofile     |\
+		"$ZDOTDIR"      )
+			return 1
+			;;
+		*)
+			return 0
+			;;
+	esac
+}
+
+_check_xdgbase() {
+	for d do
+		eval "d=\$$d"
+		if ! _is_spooky "$d"; then
+			_error "Something is fishy here, bailing out..."
+		fi
+	done
+}
+
+_check_userdir() {
+	if [ ! -f "$CONFIGDIR"/user-dirs.dirs ]; then
+		return 1
+	elif [ ! -f "$CONFIGDIR"/.user-dirs.dirs.safe ]; then
+		# we need to sanatize what is going to be sourced
+		safedirs="$(LC_ALL=C \
+			sed -e '/$(/d' \
+			-e '/`/d' \
+			-e '/^[[:space:]]/d' \
+			-e '/#/d' -e '/;/d' \
+			"$CONFIGDIR"/user-dirs.dirs \
+			| grep -o '^.*=.*'
+		)"
+		echo "$safedirs" > "$CONFIGDIR"/.user-dirs.dirs.safe
+	fi
+
+	. "$CONFIGDIR"/.user-dirs.dirs.safe
+
+	dir="$(eval echo \$XDG_$1_DIR)"
+	if [ -z "$dir" ]; then
+		return 1
+	elif _is_spooky "$dir"; then
+		return 1
+	fi
+	echo "$dir"
+}
+
+_make_fakehome() {
+	if [ -z "$FAKEHOME" ]; then
+		FAKEHOME="$(dirname "$TARGET")/$APPNAME.home"
+	fi
+
+	mkdir -p "$FAKEHOME"
+	if [ ! -w "$FAKEHOME" ]; then
+		_error "Cannot make sandboxed home at $FAKEHOME"
+	fi
+}
+
+_is_appimage() {
+	if head -c 5242880 "$1" | grep -qa 'DWARFS'; then
+		DWARFS_APPIMAGE=1
+		return 0
+	elif head -c 5242880 "$1" | grep -qa 'squashfs'; then
+		SQUASHFS_APPIMAGE=1
+		return 0
+	else
+		return 1
+	fi
+}
+
+_find_offset() {
+	offset="$(LC_ALL=C readelf -h "$1" | awk '
+		NR==13 {offset=$5}
+		NR==18 {size=$5}
+		NR==19 {count=$5}
+		END {print offset + size * count}'
+	)"
+	if [ -z "$offset" ]; then
+		_error "Not able to find offset of $1"
+	fi
+}
+
+_make_mountpoint() {
+	hash1="$(head -c 3145728 "$1" | md5sum | cut -c 1-6)"
+	hash2="$(tail -c 3145728 "$1" | md5sum | cut -c 1-6)"
+	if [ -z "$hash1" ] || [ -z "$hash2" ]; then
+		_error "ERROR: Something went wrong getting hash from $1"
+	fi
+
+	MOUNT_POINT="$TMPDIR/.$(basename "$1")-$hash1-$hash2"
+	mkdir -p "$MOUNT_POINT"
+
+	if [ "$DWARFS_APPIMAGE" = 1 ]; then
+		dwarfs -o offset="$offset" "$TARGET" "$MOUNT_POINT" || true
+	elif [ "$SQUASHFS_APPIMAGE" = 1 ]; then
+		squashfuse -o offset="$offset" "$TARGET" "$MOUNT_POINT" || true
+	fi
+}
+
+_make_bwrap_array() {
+	set -u
+	set -- \
+	  --dir /app                  \
+	  --perms 0700                \
+	  --dir /run/user/"$ID"       \
+	  --bind "$FAKEHOME" "$HOME"  \
+	  --dev /dev                  \
+	  --proc /proc                \
+	  --unshare-user-try          \
+	  --unshare-pid               \
+	  --unshare-uts               \
+	  --die-with-parent           \
+	  --unshare-cgroup-try        \
+	  --new-session               \
+	  --unshare-ipc               \
+	  --dev-bind-try /dev /dev    \
+	  --setenv  TMPDIR    /tmp    \
+	  --setenv  HOME      "$HOME" \
+	  --ro-bind "$TARGET"   /app/"$APPNAME" \
+	  --setenv XDG_RUNTIME_DIR  /run/user/"$ID"
+
+	for d in $DEFAULT_SYS_DIRS; do
+		set -- "$@" --ro-bind-try "$d" "$d"
+	done
+
+	if [ "$SQUASHFS_APPIMAGE" = 1 ] || [ "$DWARFS_APPIMAGE" = 1 ]; then
+		set -- "$@" \
+		  --setenv APPIMAGE  "$APP_APPIMAGE" \
+		  --setenv APPDIR    "$MOUNT_POINT"  \
+		  --setenv ARGV0     "$APP_ARGV0"    \
+		  --setenv APPIMAGE_EXTRACT_AND_RUN 1
+	fi
+	if [ "$SHARE_APP_DBUS" = 1 ]; then
+		set -- "$@" \
+		  --ro-bind-try /var/lib/dbus  /var/lib/dbus  \
+		  --ro-bind-try "$RUNDIR"/bus  /run/user/"$ID"/bus
+	fi
+	if [ "$SHARE_APP_AUDIO" = 1 ]; then
+		set -- "$@" \
+		  --dev-bind-try /dev/snd       /dev/snd \
+		  --ro-bind-try "$RUNDIR"/pulse /run/user/"$ID"/pulse
+	fi
+	if [ "$SHARE_APP_PIPEWIRE" = 1 ]; then
+		set -- "$@" \
+		  --ro-bind-try "$RUNDIR"/pipewire-0 /run/user/"$ID"/pipewire-0
+	fi
+	if [ "$SHARE_APP_XDISPLAY" = 1 ]; then
+		set -- "$@" \
+		  --setenv XAUTHORITY "$HOME"/.Xauthority     \
+		  --ro-bind-try /tmp/.X11-unix /tmp/.X11-unix \
+		  --ro-bind-try "$XAUTHORITY" "$HOME"/.Xauthority
+	fi
+	if [ "$SHARE_APP_WDISPLAY" = 1 ]; then
+		set -- "$@" \
+		  --setenv WAYLAND_DISPLAY  "$WDISPLAY" \
+		  --ro-bind-try "$RUNDIR"/"$WDISPLAY" /run/user/"$ID"/wayland-0
+	fi
+	if [ "$SHARE_APP_NETWORK" = 1 ]; then
+		set -- "$@" --share-net
+	else
+		set -- "$@" --unshare-net
+	fi
+	if [ "$SHARE_APP_THEME" = 1 ]; then
+		set -- "$@" \
+		  --ro-bind-try "$CONFIGDIR"/fontconfig "$CONFIGDIR"/fontconfig \
+		  --ro-bind-try "$CONFIGDIR"/gtk-3.0    "$CONFIGDIR"/gtk-3.0    \
+		  --ro-bind-try "$CONFIGDIR"/gtk-4.0    "$CONFIGDIR"/gtk-4.0    \
+		  --ro-bind-try "$CONFIGDIR"/qt5ct      "$CONFIGDIR"/qt5ct      \
+		  --ro-bind-try "$CONFIGDIR"/qt6ct      "$CONFIGDIR"/qt6ct      \
+		  --ro-bind-try "$CONFIGDIR"/Kvantum    "$CONFIGDIR"/Kvantum    \
+		  --ro-bind-try "$CONFIGDIR"/kdeglobals "$CONFIGDIR"/kdeglobals \
+		  --ro-bind-try "$CONFIGDIR"/lxde       "$CONFIGDIR"/lxde       \
+		  --ro-bind-try "$DATADIR"/themes       "$DATADIR"/themes       \
+		  --ro-bind-try "$DATADIR"/icons        "$DATADIR"/icons        \
+		  --ro-bind-try "$CONFIGDIR"/dconf      "$CONFIGDIR"/dconf      \
+		  --ro-bind-try "$CONFIGDIR"/gtk3.0     "$CONFIGDIR"/gtk3.0     \
+		  --ro-bind-try "$CONFIGDIR"/gtk4.0     "$CONFIGDIR"/gtk4.0     \
+		  --ro-bind-try "$CONFIGDIR"/kdeglobals "$CONFIGDIR"/kdeglobals \
+		  --ro-bind-try "$CONFIGDIR"/qt5ct      "$CONFIGDIR"/qt5ct      \
+		  --ro-bind-try "$CONFIGDIR"/qt6ct      "$CONFIGDIR"/qt6ct      \
+		  --ro-bind-try "$CONFIGDIR"/Kvantum    "$CONFIGDIR"/Kvantum
+	fi
+	if [ "$SHARE_APP_CONFIG" = 1 ]; then
+		set -- "$@" \
+		  --ro-bind-try "$DATADIR"/"$APPNAME"   "$DATADIR"/"$APPNAME"  \
+		  --ro-bind-try "$CACHEDIR"/"$APPNAME"  "$CACHEDIR"/"$APPNAME" \
+		  --ro-bind-try "$CONFIGDIR"/"$APPNAME" "$CONFIGDIR"/"$APPNAME"
+	fi
+
+	for d in $XDG_USER_DIRS $XDG_BASE_DIRS; do
+		ALLOW_VAR="ALLOW_$d"
+		eval "ALLOW_VAR=\$$ALLOW_VAR"
+		eval "d=\$$d"
+		case "$ALLOW_VAR" in
+			1) set -- "$@" --ro-bind-try "$d" "$d";;
+			2) set -- "$@" --bind-try    "$d" "$d";;
+		esac
+	done
+
+	while read -r d; do
+		if printf "$d" | grep -q ':rw'; then
+			set -- "$@" --bind-try    "${d%%:*}" "${d%%:*}"
+		elif [ -n "${d%%:*}" ]; then
+			set -- "$@" --ro-bind-try "${d%%:*}" "${d%%:*}"
+		fi
+	done <<-EOF
+	$ADD_DIR
+	EOF
+
+	BWRAP_ARRAY=$(_save_array "$@")
+	set +u
+}
+
+
+# check dependencies
+_dep_check $DEPENDENCIES
+
+# Make sure we always have the real home
+USER="${LOGNAME:-${USER:-${USERNAME:-$(id -run)}}}"
+HOME="$(getent passwd "$USER" | cut -d':' -f6)"
+ID="$(id -u)"
+
+# get xdg vars
+BINDIR="${XDG_BIN_HOME:-~/.local/bin}"
+DATADIR="${XDG_DATA_HOME:-~/.local/share}"
+CONFIGDIR="${XDG_CONFIG_HOME:-~/.config}"
+CACHEDIR="${XDG_CACHE_HOME:-~/.cache}"
+STATEDIR="${XDG_STATE_HOME:-~/.local/state}"
+RUNDIR="${XDG_RUNTIME_DIR:-/run/user/"$ID"}"
+
+# check xdg user dirs, if they are spooky we use their default value
+APPLICATIONSDIR="$(_check_userdir APPLICATIONS || echo ~/Applications)"
+DESKTOPDIR="$(     _check_userdir DESKTOP      || echo ~/Desktop)"
+DOCUMENTSDIR="$(   _check_userdir DOCUMENTS    || echo ~/Documents)"
+DOWNLOADDIR="$(    _check_userdir DOWNLOAD     || echo ~/Downloads )"
+GAMESDIR="$(       _check_userdir GAMES        || echo ~/Games)"
+MUSICDIR="$(       _check_userdir MUSIC        || echo ~/Music)"
+PICTURESDIR="$(    _check_userdir PICTURES     || echo ~/Pictures)"
+PUBLICSHAREDIR="$( _check_userdir PUBLICSHARE  || echo ~/Public)"
+TEMPLATESDIR="$(   _check_userdir TEMPLATES    || echo ~/Templates)"
+VIDEOSDIR="$(      _check_userdir VIDEOS       || echo ~/Videos)"
+
+# check xdg base dir vars are not some odd value
+_check_xdgbase $XDG_BASE_DIRS
+
+ZDOTDIR="${ZDOTDIR:-$HOME}"
+TMPDIR="${TMPDIR:-/tmp}"
+WDISPLAY="${WAYLAND_DISPLAY:-wayland-0}"
+XDISPLAY="${XAUTHORITY:-$RUNDIR/Xauthority}"
+
+# parse the array
+while :; do
+	case "$1" in
+		--help|-h|-H|'')
+			_help
+			;;
+		--version|-v|-V)
+			echo "$VERSION"
+			exit 0
+			;;
+		--no-theme)
+			SHARE_APP_THEME=0
+			shift
+			;;
+		--no-config)
+			SHARE_APP_CONFIG=0
+			shift
+			;;
+		--no-display)
+			SHARE_APP_DISPLAY=0
+			shift
+			;;
+		--data-dir|--sandboxed-home)
+			shift
+			FAKEHOME="$(readlink -f "$1")"
+			if ! _is_spooky "$FAKEHOME"; then
+				_error "Cannot use $1 as sandboxed home"
+			fi
+			shift
+			;;
+		--add-file|--add-dir)
+			case "$2" in
+				xdg-applications:rw) ALLOW_APPLICATIONSDIR=2;;
+				xdg-applications*)   ALLOW_APPLICATIONSDIR=1;;
+				xdg-desktop:rw)      ALLOW_DESKTOPDIR=2     ;;
+				xdg-desktop*)        ALLOW_DESKTOPDIR=1     ;;
+				xdg-documents:rw)    ALLOW_DOCUMENTSDIR=2   ;;
+				xdg-documents*)      ALLOW_DOCUMENTSDIR=1   ;;
+				xdg-download:rw)     ALLOW_DOWNLOADDIR=2    ;;
+				xdg-download*)       ALLOW_DOWNLOADDIR=1    ;;
+				xdg-games:rw)        ALLOW_GAMESDIR=2       ;;
+				xdg-games*)          ALLOW_GAMESDIR=1       ;;
+				xdg-music:rw)        ALLOW_MUSICDIR=2       ;;
+				xdg-music*)          ALLOW_MUSICDIR=1       ;;
+				xdg-pictures:rw)     ALLOW_PICTURESDIR=2    ;;
+				xdg-pictures*)       ALLOW_PICTURESDIR=1    ;;
+				xdg-publicshare:rw)  ALLOW_PUBLICSHAREDIR=2 ;;
+				xdg-publicshare*)    ALLOW_PUBLICSHAREDIR=1 ;;
+				xdg-templates:rw)    ALLOW_TEMPLATESDIR=2   ;;
+				xdg-templates*)      ALLOW_TEMPLATESDIR=1   ;;
+				xdg-videos:rw)       ALLOW_VIDEOSDIR=2      ;;
+				xdg-videos*)         ALLOW_VIDEOSDIR=1      ;;
+				xdg-bin:rw)          ALLOW_BINDIR=2         ;;
+				xdg-bin*)            ALLOW_BINDIR=1         ;;
+				xdg-cache:rw)        ALLOW_CACHEDIR=2       ;;
+				xdg-cache*)          ALLOW_CACHEDIR=1       ;;
+				xdg-config:rw)       ALLOW_CONFIGDIR=2      ;;
+				xdg-config*)         ALLOW_CONFIGDIR=1      ;;
+				xdg-data:rw)         ALLOW_DATADIR=2        ;;
+				xdg-data*)           ALLOW_DATADIR=1        ;;
+				xdg-rundir:rw)       ALLOW_RUNDIR=2         ;;
+				xdg-rundir*)         ALLOW_RUNDIR=1         ;;
+				xdg-statedir:rw)     ALLOW_STATEDIR=2       ;;
+				xdg-statedir*)       ALLOW_STATEDIR=1       ;;
+				''|-*)
+					_error "No file or directory given $1"
+					;;
+				# Store each extra file/dir in a new line
+				# new POSIX doesn't allow newline characters
+				# in filenames, which is very useful here
+				*)
+					ADD_DIR="$ADD_DIR
+					$(readlink -f "$2")"
+					;;
+			esac
+			shift
+			shift
+			;;
+		--rm-socket)
+			case "$2" in
+				alsa |\
+				audio|\
+				pulseaudio) SHARE_APP_AUDIO=0   ;;
+				pipewire)   SHARE_APP_PIPEWIRE=0;;
+				dbus)       SHARE_APP_DBUS=0    ;;
+				network)    SHARE_APP_NETWORK=0 ;;
+				x11)        SHARE_APP_XDISPLAY=0;;
+				wayland)    SHARE_APP_WDISPLAY=0;;
+				'')
+					_error "No socket given to $1"
+					;;
+				*)
+					_error "Unknown socket specified"
+					;;
+			esac
+			shift
+			shift
+			;;
+		--rm-file|--rm-dir)
+			case "$2" in
+				''|-*)
+					_error "No file or directory given $1"
+					;;
+				*)
+					DEFAULT_SYS_DIRS="$(echo \
+					  "$DEFAULT_SYS_DIRS" | grep -Fvw "$2"
+					)"
+					;;
+			esac
+			shift
+			shift
+			;;
+		--)
+			shift
+			;;
+		-*)
+			_error "Unknown option: $1"
+			;;
+		*|'')
+			if _is_target "$1"; then
+				# We shift and break here to later pass
+				# the rest of the array to $TO_EXEC
+				shift
+			else
+				_error "Cannot find application to sandbox"
+			fi
+			break
+			;;
+	esac
+done
+
+_make_fakehome
+
+# Check if the app is an appimage, if so find offset and mount
+if _is_appimage "$TARGET"; then
+	_find_offset     "$TARGET"
+	_make_mountpoint "$TARGET"
+fi
+
+if [ -n "$MOUNT_POINT" ]; then
+	if [ -f "$MOUNT_POINT"/AppRun ]; then
+		TO_EXEC="$MOUNT_POINT"/AppRun
+	elif [ -f "$MOUNT_POINT"/Run ]; then
+		# runimage uses Run instead of AppRun
+		TO_EXEC="$MOUNT_POINT"/Run
+	else
+		_error "$TARGET does not contain an AppRun or Run file"
+	fi
+else
+	TO_EXEC="$TARGET"
+fi
+
+# Save current array and make bwrap array
+ARRAY=$(_save_array "$TO_EXEC" "$@")
+_make_bwrap_array
+
+# Now merge the arrays
+eval set -- "$BWRAP_ARRAY" -- "$ARRAY"
+
+# Do the thing!
+bwrap "$@"
+
