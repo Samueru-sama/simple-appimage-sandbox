fe0033fb0cbc1d527891a92554d7743947804a7e
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-05-30 00:17:06 -0400

    remove `readelf` dependency
    big credits to @xplshn

--- sas.sh
+++ sas.sh
@@ -54,7 +54,6 @@ DEPENDENCIES="
 	grep
 	head
 	md5sum
-	readelf
 	readlink
 	sed
 	squashfuse
@@ -241,11 +240,50 @@ _is_appimage() {
 }
 
 _find_offset() {
-	offset="$(LC_ALL=C readelf -h "$1" | awk '
-		NR==13 {offset=$5}
-		NR==18 {size=$5}
-		NR==19 {count=$5}
-		END {print offset + size * count}'
+	offset="$(LC_ALL=C od -An -vtx1 -N 64 -- "$1" | awk '
+	BEGIN {
+		  for (i = 0; i < 16; i++) {
+		  c = sprintf("%x", i)
+		  H[c] = i
+		  H[toupper(c)] = i
+		  }
+	  }
+	  {
+		  elfHeader = elfHeader " " $0
+	  }
+	  END {
+		  $0 = toupper(elfHeader)
+		  if ($5 == "02") is64 = 1; else is64 = 0
+		  if ($6 == "02") isBE = 1; else isBE = 0
+		  if (is64) {
+		  if (isBE) {
+			  shoff = $41 $42 $43 $44 $45 $46 $47 $48
+			  shentsize = $59 $60
+			  shnum = $61 $62
+		  } else {
+			  shoff = $48 $47 $46 $45 $44 $43 $42 $41
+			  shentsize = $60 $59
+			  shnum = $62 $61
+		  }
+		  } else {
+		  if (isBE) {
+			  shoff = $33 $34 $35 $36
+			  shentsize = $47 $48
+			  shnum = $49 $50
+		  } else {
+			  shoff = $36 $35 $34 $33
+			  shentsize = $48 $47
+			  shnum = $50 $49
+		  }
+		  }
+		  print parsehex(shoff) + parsehex(shentsize) * parsehex(shnum)
+		  }
+	  function parsehex(v,    i, r) {
+		  r = 0
+		  for (i = 1; i <= length(v); i++)
+		  r = r * 16 + H[substr(v, i, 1)]
+		  return r
+	  }'
 	)"
 	if [ -z "$offset" ]; then
 		_error "Not able to find offset of $1"
